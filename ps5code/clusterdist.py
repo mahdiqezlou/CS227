b6070921fd3ae97496d3652172d875384a1a005a
mahdi <mahdiqezlou@gmail.com>
2020-02-09 15:01:14 -0800

    ps5 added

---
+++ ps5code/clusterdist.py
@@ -0,0 +1,47 @@
+from factor import *
+import copy
+
+# describes a distribution over a cluster graph
+# as parameterized by betas (factors over the clusters)
+# and mus (factors over the sep-sets)
+class clusterdist:
+    # U is the clustergraph on which this distribution is based
+    # alpha is a mapping from factors to cluster indexes in U
+    def __init__(self,U,alpha):
+        self._U = copy.copy(U) # so that the original can change
+        self._beta = [] # list of local factors, _beta[i] matches cluster i in U
+        self._mu = {} # dictionary mapping (i,j) to factor for edge i-j
+                # note that (3,5) and (5,3) are the same, so we only
+                # keep (3,5) -- because 3<5
+        self._initializegraph(alpha)
+
+    # assumes that sep-set is intersection of scopes of either side
+    def _initializegraph(self,alpha):
+        for i in range(len(self._U.clusters)):
+            self._beta.append(discretefactor(self._U.clusters[i],1.0))
+        for i in range(len(self._U.clusters)):
+            for j in self._U.adj(i):
+                if i<j:
+                    self._mu[(i,j)] = discretefactor(self._U.clusters[i] & self._U.clusters[j],1.0)
+        for f,i in alpha.items():
+            self._beta[i] = self._beta[i] * f
+
+    @property
+    def graph(self):
+        return self._U
+
+    # after calibration, should be the marginal over the cluster i
+    def getbeta(self,i):
+        return self._beta[i]
+
+    # after calibration, should be the marginal over the sep-set btwn i&j
+    def getmu(self,i,j):
+        return self._mu[(i,j)] if i<j else self._mu[(j,i)]
+
+    # calibrates, assuming that the clustergraph is a *forest*
+    # (not necessarily a tree, despite the name -- it might be
+    #  multiple disconnected trees -- this happens often once
+    #  evidence is introduced)
+    def treecalibrate(self):
+        ### For you to write (along with any helper methods you wish)
+        pass
---
+++ ps5code/testct.py
@@ -0,0 +1,98 @@
+from clustergraph import *
+from clusterdist import *
+from factor import *
+from factorset import *
+from itertools import product
+from veinf import *
+
+def makefactor(vars,vals):
+    phi = discretefactor(set(vars))
+    for j,x in enumerate(product(*map((lambda v : [(v,i) for i in range(v.nvals)]),vars))):
+        s = {a:b for (a,b) in x}
+        phi[s] = vals[j]
+    return phi
+
+def buildnewstudentex():
+    c = discretevariable("c",2)
+    d = discretevariable("d",2)
+    t = discretevariable("t",2)
+    i = discretevariable("i",2)
+    g = discretevariable("g",3)
+    s = discretevariable("s",2)
+    l = discretevariable("l",2)
+    j = discretevariable("j",2)
+
+    studentbn = factorset()
+
+    pc = makefactor([c],[0.5,0.5])
+    studentbn.addfactor(pc)
+    pd = makefactor([c,d],[0.4,0.6,0.8,0.2])
+    studentbn.addfactor(pd)
+    pi = makefactor([i],[0.6,0.4])
+    studentbn.addfactor(pi)
+    pt = makefactor([i,t],[0.9,0.1,0.4,0.6])
+    studentbn.addfactor(pt)
+    pg = makefactor([t,d,g],
+        [0.3,0.4,0.3,
+         0.05,0.25,0.7,
+         0.9,0.08,0.02,
+         0.5,0.3,0.2])
+    studentbn.addfactor(pg)
+    ps = makefactor([t,s],[0.95,0.05,0.2,0.8])
+    studentbn.addfactor(ps)
+    pl = makefactor([g,l],[0.1,0.9,0.4,0.6,0.99,0.01])
+    studentbn.addfactor(pl)
+    pj = makefactor([l,s,j],
+        [0.9,0.1,
+            0.4,0.6,
+            0.3,0.7,
+            0.1,0.9])
+    studentbn.addfactor(pj)
+
+    cg = clustergraph()
+    cd = cg.addcluster({c,d})
+    gdt = cg.addcluster({g,d,t})
+    gts = cg.addcluster({g,t,s})
+    it = cg.addcluster({i,t})
+    slg = cg.addcluster({s,l,g})
+    jls = cg.addcluster({j,l,s})
+    cg.addedge(cd,gdt)
+    cg.addedge(gdt,gts)
+    cg.addedge(slg,gts)
+    cg.addedge(slg,jls)
+    cg.addedge(gts,it)
+    alpha = {pc : cd, pd : cd, pg : gdt, pi : it, pt : it, ps : gts, pl : slg, pj : jls}
+
+    return studentbn,(c,d,t,i,g,s,l,j),cg,alpha
+
+# needed because str() on a set uses repr(), not str() on underlying elements
+def settostr(s):
+    return '{'+','.join([str(i) for i in s])+'}'
+
+def printdist(cd):
+    cg = cd.graph
+    for i,f in enumerate(cg.clusters):
+        print("beta for %s:" % settostr(f))
+        print(cd.getbeta(i))
+    for i in range(len(cg.clusters)):
+        for j in cg.adj(i):
+            if j>i:
+                print("mu for (%s)-(%s):" % (settostr(cg.clusters[i]),settostr(cg.clusters[j])))
+                print(cd.getmu(i,j))
+
+
+## This is just one very simple example.  You'll need to test to 
+## make sure your code works on other more complex examples
+## including those with disconnected graphs (as often happens after
+## conditioning on evidence)
+##
+##
+if __name__ == '__main__':
+    studentex,(c,d,t,i,g,s,l,j),cg,alpha = buildnewstudentex()
+    cd = clusterdist(cg,alpha)
+
+    print ("before calibration:")
+    printdist(cd)
+    cd.treecalibrate()
+    print ("after calibration:")
+    printdist(cd)
